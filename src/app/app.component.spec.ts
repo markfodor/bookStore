0b321ece1300d77c5c89e1d86c4c72cf4b99316e
markfodor <piciusmarcus@gmail.com>
2017-12-15 01:17:58 +0100

    fix(test) add FormsModule to fix failing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,13 @@ import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {BookService} from './service/book.service';
+import {FormsModule} from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        FormsModule,
         HttpClientModule
       ],
       declarations: [
